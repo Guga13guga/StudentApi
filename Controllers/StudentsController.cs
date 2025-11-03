4016281a392a900d955a5acb53008aaf34c4d638
STEP <step@gmail.com>
2025-11-03 21:52:14 +0400

    Update add student conntroller

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApi.Database;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly StudentsDbContext _context;
+
+    public StudentsController(StudentsDbContext context)
+    {
+        _context = context;
+    }
+    //post, put, get, delete, head ,path
+
+    [HttpPost("AddNewStudent")]
+    public IActionResult AddNewStudent([FromBody]Students student)
+    {
+        try
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return Ok("Warmatebit chaiwera");
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+            throw;
+        }
+    }
+
+    [HttpGet("GetAllStudents")]
+    public IActionResult GetStudents()
+    {
+        var allStudents = _context.Students;
+        if (allStudents.Any())
+        {
+            return Ok(allStudents);
+        }
+        else
+        {
+            return NotFound("Studentis sia carielia");
+        }
+    }
+
+    [HttpDelete("/{id}")]
+    public IActionResult DeleteStudentById(int id)
+    {
+        try
+        {
+            var student = _context.Students.FirstOrDefault(x => x.Id == id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+                return Ok("studenti wavshale");
+            }
+            return NotFound("msgavi studenti ar arsebobs");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
+        }
+    }
+
+}
